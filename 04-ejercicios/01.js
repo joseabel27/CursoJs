e21c73d3d9662a527193b6cc04fb74483999fc54
Jose Romero <parrajose27@hotmail.com>
2025-05-23 13:20:33 -0500

    agregando

---
+++ 04-ejercicios/01.js
@@ -0,0 +1,26 @@
+/* Construye una funcion que reciba 2 parametros e indique cual de los 2 numeros es mayor */
+
+/* function numeroMayor(a , b ){
+
+    if ( a > b){
+
+        return a;
+    } else {
+
+        return b;
+    }
+
+     
+}
+
+let mayor = numeroMayor (10 , 5);
+console.log(mayor);
+ */
+
+
+function cualEsMayor(a, b){
+    return (a > b) ? a : b;
+}
+
+let mayor = cualEsMayor(10,23);
+console.log(mayor);
