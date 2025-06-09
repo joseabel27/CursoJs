e762b87a44ecd86ce7e3bea659fd364ef94d7e86
Jose Romero <parrajose27@hotmail.com>
2025-06-09 13:17:10 -0500

    agregando ejercicios

---
+++ 04-ejercicios/04.js
@@ -0,0 +1,14 @@
+/* Realizar un algoritmo que solo imprima los numeros impares del 0 al 10 */
+
+let i = 0;
+
+while (i <=10){
+
+    if (i % 2 !== 0){
+
+        console.log('El Numero es impar',i);
+    }
+
+    i++
+}
+
---
+++ 04-ejercicios/05.js
@@ -0,0 +1,2 @@
+/* Crear un algoritmo que devulva el numero menor y mayor de un Array */
+
