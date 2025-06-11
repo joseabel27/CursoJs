904b18194b7d680729b19d9d2f61a0c55d356057
Jose Romero <parrajose27@hotmail.com>
2025-06-11 17:49:41 -0500

    agregando 4 ejercicio

--- 04-ejercicios/05.js
+++ 04-ejercicios/05.js
@@ -1,2 +1,19 @@
 /* Crear un algoritmo que devulva el numero menor y mayor de un Array */
 
+let array =[2,5,7,15,-5,-100,55];
+
+function getMenorMayor(arr){
+
+    let menor = arr[0];
+    let mayor = arr[0];
+
+    for (numero of arr){
+
+        menor = menor < numero ? menor : numero;
+        mayor = mayor > numero ? mayor : numero;
+    }
+    return [menor, mayor];
+}
+
+let numeros = getMenorMayor(array)
+console.log(numeros);
