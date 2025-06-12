8c210bd461dcd0c6888bc55af94032a372efc9b8
Jose Romero <parrajose27@hotmail.com>
2025-06-12 13:13:00 -0500

    agregando ejercicio 6

---
+++ 04-ejercicios/06.js
@@ -0,0 +1,22 @@
+/* Crear un algoritmo que devuelva la cantidad de numeos positivos de un array */
+
+let array = [2, 5, 7, 15, -5, -100, 55];
+
+function cuantosPositivos(arr){
+
+let cantidad = 0;
+
+for (elemento of arr){
+
+    if(elemento > 0){
+
+        cantidad++;
+    }
+}
+
+return cantidad;
+
+}
+
+let resultado = cuantosPositivos(array);
+console.log(resultado);
